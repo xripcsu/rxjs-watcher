fd3313526f8a94bcfbc7c92cfb7ded1812700656
Ashwin Menkudle <ashwin.menkudle@gmail.com>
2020-04-11 13:29:33 +0530

    Provision to access overlapping value
    values produced using from and of operator overlapped with each other.

--- extension/page/components/marble/marble.js
+++ extension/page/components/marble/marble.js
@@ -23,6 +23,7 @@ export class Marble extends HTMLElement {
         this.duration;
         this.started = false;
         this.finishedWithError = false;
+        this.overlapCount = 0;
     }
 
     set name(name) {
@@ -39,7 +40,8 @@ export class Marble extends HTMLElement {
     }
 
     move(currentTime) {
-        if(!this.started) {
+        this.overlapCount = 0;
+        if (!this.started) {
             this.message = '';
         }
         const lineWidth = (currentTime - this.startTime) / this.duration;
@@ -55,6 +57,10 @@ export class Marble extends HTMLElement {
         const left = (new Date().getTime() - this.startTime) / this.duration;
         const el = document.createElement('div')
         el.style.left = `calc(${left}% - ${offset}px)`;
+        if (type === 'value') {
+            el.style.top = `${this.overlapCount * 15}px`
+        }
+        this.overlapCount++;
         el.className = `point ${type}`;
         el.innerHTML = content;
         this.lineWidth = left;
